d0ce7a6b96f6192e9d974f84af96d8b2612fa33d
Andi Dwi Prastyo <2141720046@student.polinema.ac.id>
2024-04-06 16:24:00 +0700

    fix: Logo displayed properly

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import Image from 'next/image';
 import '../styles/index.scss';
+import eeprom from '../../public/logo_eeprom.svg';
 
 const Header: React.FC = () => {
   return (
     <header className="header">
       <div className="header-content">
         <div className="logo">
-          <Image src="/../public/logo_eeprom.svg" width={40} height={40} alt="EEPROM Logo" />
+          <Image src={eeprom} width={40} height={40} alt="EEPROM Logo" />
           <span>EEPROM Polinema</span>
         </div>
         <nav className="navbar">
           <ul className="nav-list">
             <li className="nav-item">Home</li>
-            <li className="nav-item">Profil</li>
-            <li className="nav-item">Artikel</li>
-            <li className="nav-item">Hubungi Kami</li>
+            <li className="nav-item">Profile</li>
+            <li className="nav-item">Article</li>
+            <li className="nav-item">Contact us</li>
           </ul>
         </nav>
       </div>
