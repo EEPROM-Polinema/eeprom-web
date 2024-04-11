ba8c3d1a127f5d6098b33b5b35cfc75fe1607b23
andidprastyo <2141720046@student.polinema.ac.id>
2024-04-11 18:05:17 +0700

    style: change to tailwindcss

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,35 +1,42 @@
-import React from 'react';
+import React from "react";
+import Link from "next/link";
 import Image from 'next/image';
-import '../styles/header.scss';
-import eeprom from '../../public/logo_eeprom.svg';
+import eeprom from "../../public/logo_eeprom.svg";
 
 const Header: React.FC = () => {
   return (
-    <header className="header">
-      <div className="header-content">
-        <div className="logo">
-          <Image src={eeprom} width={40} height={40} alt="EEPROM Logo" />
-          <span>EEPROM Polinema</span>
+    <header className="bg-gray-100 py-4">
+      <div className="container mx-auto flex justify-between items-center px-4">
+        <div className="flex items-center">
+          <Link href="/">
+            <div className="flex items-center ml-8">
+              <Image src={eeprom} width={40} height={40} alt="EEPROM Logo" />
+              <span className="text-gray-600 text-xl cursor-pointer ml-2">EEPROM Polinema</span>
+            </div>
+          </Link>
         </div>
-        <nav className="navbar">
-          <ul className="nav-list">
-            <li className="nav-item">Home</li>
-            <li className="nav-item dropdown">
-              Profile
-              <div className="dropdown-content">
-                <a href="#">Visi & Misi</a>
-                <a href="#">Divisi</a>
-                <a href="#">Struktur Organisasi</a>
-              </div>
+        <nav>
+          <ul className="flex space-x-4">
+            <li className="px-2">
+              <Link href="/">
+                <span className="text-gray-900 hover:text-cyan-600 cursor-pointer">Home</span>
+              </Link>
             </li>
-            <li className="nav-item dropdown">
-              Article
-              <div className="dropdown-content">
-                <a href="#">Program Kerja</a>
-                <a href="#">Prestasi</a>
-              </div>
+            <li className="px-2">
+              <Link href="/about">
+                <span className="text-gray-900 hover:text-cyan-600 cursor-pointer">Profile</span>
+              </Link>
             </li>
-            <li className="nav-item">Contact us</li>
+            <li className="px-2">
+              <Link href="/about">
+                <span className="text-gray-900 hover:text-cyan-600 cursor-pointer">Article</span>
+              </Link>
+            </li>
+            <li className="px-2">
+              <Link href="/contact">
+                <span className="text-gray-900 hover:text-cyan-600 cursor-pointer">Contact us</span>
+              </Link>
+            </li>
           </ul>
         </nav>
       </div>
